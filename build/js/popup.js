22b7d1ddb070a6f951f261511c9cf13b18d01e26
김광일 <54162245+pangil5634@users.noreply.github.com>
2026-09-09 21:55:00 +0900

    fix: skip intro popup window on mobile
    모바일에서는 새 창 팝업이 차단되거나 별도 탭으로 열려 흐름이 끊기므로, 좁은 화면에서는 소개 팝업을 띄우지 않도록 했습니다.
    opener가 없는 상태에서 버튼을 눌러도 동작하도록 현재 창에서 이동하는 폴백을 추가했습니다.

--- build/js/popup.js
+++ build/js/popup.js
@@ -1,14 +1,18 @@
 function showPopup() {
-    open("popup.html", "popup", "width=500, height=500, left=470, top=150");
+    // 모바일에서는 새 창 팝업이 탭으로 열리거나 차단되므로 띄우지 않는다
+    if (window.innerWidth <= 768) {
+        return;
+    }
+    open("popup.html", "popup", "width=500, height=560, left=470, top=150");
 }
 
 function goMenual() {
 
-    // opener.location.href = "http://www.naver.com";
-    opener.location.href = 'introduce.html';
-
-    window.close();
-
-
+    if (window.opener && !window.opener.closed) {
+        window.opener.location.href = 'introduce.html';
+        window.close();
+    } else {
+        location.href = 'introduce.html';
+    }
 
 }
